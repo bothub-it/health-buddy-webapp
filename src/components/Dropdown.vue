3e2cb07b675f57584c29bb7b5e3c7f344a495328
Marcos Paulo <marcos182p@gmail.com>
2020-04-10 19:48:02 -0300

    Adjusting navbar

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,9 +1,9 @@
 <template>
-    <div :class="['dropdown', dropdownActive ? 'is-active' : '']">
+    <div :class="{ 'is-active': dropdownActive }" class="dropdown">
         <div @click="dropdownActive = !dropdownActive" >
             <slot name="trigger" />
         </div>
-        <div @click="dropdownActive = false">
+        <div @click="dropdownActive = false" class="dropdown-menu">
             <slot name="options" />
         </div>
     </div>
@@ -11,18 +11,34 @@
 
 <script>
 export default {
-    name: 'Dropdown',
-    data() {
-        return {
-            dropdownActive: false,
-        }
-    },
-    created() {
-        window.addEventListener('click', (e) => {
-            if (!this.$el.contains(e.target)) {
-                this.dropdownActive = false;
-            }
-        });
-    },
+  name: 'Dropdown',
+  data() {
+    return {
+      dropdownActive: false,
+    }
+  },
+  created() {
+    window.addEventListener('click', (e) => {
+      if (!this.$el.contains(e.target)) {
+        this.dropdownActive = false;
+      }
+    });
+  },
 }
 </script>
+
+<style lang="scss">
+.dropdown {
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+&-menu {
+   background-color: white;
+   border: 1px solid rgba(0,0,0,.15);
+   border-radius: .25rem;
+   position: absolute;
+   left: -20% !important;
+ }
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,110 +1,83 @@
 <template>
   <div class="navbar__wrapper">
-    <modal 
-      :active.sync="modalActive">
-      <router-link
-              v-for="item in items"
-              class="modal-item"
-              :key="item.id"
-              :to="`index#${item.id}`"
-              @click="modalActive=false"
-            >
+    <modal :active.sync="modalActive">
+      <router-link v-for="item in items"
+                   :key="item.id"
+                   :to="`index#${item.id}`"
+                   @click="modalActive=false"
+                   class="modal-item">
               {{ item.title }}
-            </router-link>
+      </router-link>
     </modal>
-    <nav
-      class="navbar is-fixed-top"
-      role="navigation"
-      aria-label="dropdown navigation"
-    >
+    <nav class="navbar"
+         role="navigation"
+         aria-label="dropdown navigation">
       <div class="navbar-brand">
-        <img
-          class="navbar-item navbar__logo"
-          src="@/assets/img/doctor-square.png"
-        >
+        <img class="navbar-item navbar__logo"
+             src="@/assets/img/doctor-square.png">
 
-        <a
-          class="navbar-item"
-          href="https://healthbuddy.info/index.html"
-        >
+        <a class="navbar-item"
+           href="https://healthbuddy.info/index.html">
           <h1 class="navbar__title"> HealthBuddy </h1>
         </a>
 
-        <a
-          role="button"
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-        >
+        <a role="button"
+           class="navbar-burger"
+           aria-label="menu"
+           aria-expanded="false">
           <dropdown class="navbar-item">
-              <a
-                class="dropdown-trigger animatable"
-                slot="trigger"
-              >
-                {{ $t('navbar.languages') }} <i class="mdi mdi-menu-down" />
-              </a>
+            <a class="dropdown-trigger animatable"
+               slot="trigger">
+              {{ $t('navbar.languages') }} <i class="mdi mdi-menu-down" />
+            </a>
 
-              <div 
-                class="dropdown-menu"
-                slot="options">
-                <a
-                  v-for="(language, index) in languages"
-                  :key="index"
-                  class="dropdown-item"
-                  @click="didClickLanguage(language.id)"
-                >
-                  {{ language.name }}
-                </a>
-              </div>
-            </dropdown>
+            <a v-for="(language, index) in languages"
+               :key="index"
+               @click="didClickLanguage(language.id)"
+               slot="options"
+               class="dropdown-item">
+              {{ language.name }}
+            </a>
+
+          </dropdown>
         </a>
 
-        <a
-          role="button"
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          @click="modalActive=true"
-        >
+        <a @click="modalActive=true"
+           role="button"
+           class="navbar-burger"
+           aria-label="menu"
+           aria-expanded="false">
           <span aria-hidden="true" />
           <span aria-hidden="true" />
           <span aria-hidden="true" />
-          
+
         </a>
       </div>
 
       <div class="navbar-end">
         <div class="navbar-menu">
           <div class="navbar-start">
-            <router-link
-              v-for="item in items" 
-              :key="item.id"
-              :to="`index#${item.id}`"
-              class="navbar-item animatable"
-            >
+            <router-link v-for="item in items"
+                         :key="item.id"
+                         :to="`index#${item.id}`"
+                         class="navbar-item animatable">
               {{ item.title }}
             </router-link>
 
             <dropdown class="navbar-item">
-              <a
-                class="dropdown-trigger animatable"
-                slot="trigger"
-              >
+              <a class="dropdown-trigger animatable"
+                 slot="trigger">
                 {{ $t('navbar.languages') }} <i class="mdi mdi-menu-down" />
               </a>
 
-              <div 
-                class="dropdown-menu"
-                slot="options">
-                <a
-                  v-for="(language, index) in languages"
-                  :key="index"
-                  class="dropdown-item"
-                  @click="didClickLanguage(language.id)"
-                >
-                  {{ language.name }}
-                </a>
-              </div>
+              <a v-for="(language, index) in languages"
+                 :key="index"
+                 @click="didClickLanguage(language.id)"
+                 slot="options"
+                 class="dropdown-item">
+                {{ language.name }}
+              </a>
+
             </dropdown>
           </div>
         </div>
@@ -141,7 +114,7 @@ export default {
             name: 'Russian',
           }
         ],
-      } 
+      }
     },
     computed: {
       ...mapGetters([
@@ -182,93 +155,81 @@ export default {
 
 <style lang="scss" scoped>
 
-  .modal-item {
-    display: block;
-    margin: 0.75rem;
+.navbar-brand {
+  align-items: center;
+}
+
+.modal-item {
+  display: block;
+  margin: 0.75rem;
+}
+
+.animatable {
+  transition: all .1s;
+}
+
+.animatable:hover {
+  font-weight: bold;
+  transform: scale(1.1);
+}
+
+.navbar {
+
+  max-width: 1140px;
+  margin: 0 auto;
+  background-color: #374EA2;
+
+  &__icon {
+    font-size: 25px;
   }
 
-  .dropdown {
-    width: 100%;
-    height: 100%;
+  &-item, .dropdown-trigger {
+    font-family: "Open Sans", sans-serif;
+    font-size: 14px;
+    color: white;
+    font-weight: 400;
+    line-height: 1.5;
+    text-align: left;
     display: flex;
-    align-items: center;
-
-    &-menu {
-      background-color: white;
-      border: 1px solid rgba(0,0,0,.15);
-      border-radius: .25rem;
-      position: absolute;
-      left: -20% !important;
-    }
+    align-content: center;
   }
 
-  .animatable {
-    transition: all .1s;
+  &-item:hover, &-burger:hover, .dropdown-trigger:hover {
+    background-color: transparent;
+    color: white;
   }
 
-  .animatable:hover {
-    font-weight: bold;
-    transform: scale(1.1);
+  &-burger {
+    color: white;
   }
 
-  .navbar {
-
-    max-width: 1140px;
-    margin: 0 auto;
+  &__wrapper {
+    left: 0;
+    position: fixed;
+    right: 0;
+    z-index: 29;
     background-color: #374EA2;
+    top: 0;
+  }
 
-    &__icon {
-      font-size: 25px;
-    }
+  &__title {
+    color: white;
+    text-decoration: none;
+    font-size: 30px;
+    line-height: 1;
+    font-weight: 400;
+    letter-spacing: 2px;
+    font-family: "Raleway", sans-serif;
 
-    &-item, .dropdown-trigger {
-      font-family: "Open Sans", sans-serif;
-      font-size: 14px;
-      color: white;
-      font-weight: 400;
-      line-height: 1.5;
-      text-align: left;
-      display: flex;
-      align-content: center;
-    }
-
-    &-item:hover, &-burger:hover, .dropdown-trigger:hover {
-      background-color: transparent;
-      color: white;
-    }
-
-    &-burger {
-      color: white;
-    }
-
-    &__wrapper {
-      left: 0;
-      position: fixed;
-      right: 0;
-      z-index: 29;
-      background-color: #374EA2;
-      height: 74px;
-      top: 0;
-    }
-
-    &__title {
-      color: white;
-      text-decoration: none;
-      font-size: 30px;
-      line-height: 1;
-      font-weight: 400;
-      letter-spacing: 2px;
-      font-family: "Raleway", sans-serif;
-
-      @media (max-width: 768px) {
-        font-size: 20px;
-      }
-    }
-
-    &__logo {
-      margin-top: 1vh;
-      max-height: 7vh;
-      border-style: none;
+    @media (max-width: 768px) {
+      font-size: 20px;
     }
   }
+
+  &__logo {
+    margin-top: 1vh;
+    max-height: 7vh;
+    border-style: none;
+  }
+}
 </style>
